da4d74f9a28c3a58c0ced31a39f9b29962f00b9b
hustaoyang <t.yang.hust@gmail.com>
2015-05-20 23:08:45 -0700

    define strcut

--- BiTree.cpp
+++ BiTree.cpp
@@ -1,2 +1,17 @@
 #include<iostream>
+#include<string>
 
+typedef struct BTNode
+{
+    char  c_value;
+    
+    BTree lchild, rchild;
+}BTNode, *BTree;
+
+
+int main(void)
+{
+    
+    cout<<"the end"<<endl;
+    return 0;
+}
