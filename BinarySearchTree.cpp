9bd54c0560f991d3f4efcf4eae0a69c564a7d502
hustaoyang <t.yang.hust@gmail.com>
2015-05-22 14:32:05 +0800

    Binary Search tree

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+typedef struct BTNode
+{
+    int i_value;
+    struct BTNode *lchild, *rchild;
+}BTNode, *BTree;
+//void InsertKey(
+int main(void)
+{
+     
+
+     return 0;
+}
