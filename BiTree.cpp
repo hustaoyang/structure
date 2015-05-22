d185e6c887d07757c22f1e5e88dd63534fcaaca9
hustaoyang <t.yang.hust@gmail.com>
2015-05-22 09:36:14 +0800

    change the structer

--- BiTree.cpp
+++ BiTree.cpp
@@ -1,17 +1,102 @@
 #include<iostream>
-#include<string>
+#include<stdlib.h>
 
-typedef struct BTNode
+using namespace std;
+
+typedef struct BTNode 
 {
-    char  c_value;
-    
-    BTree lchild, rchild;
+     char c_value;
+     struct BTNode *lchild, *rchild;
+
 }BTNode, *BTree;
+
+void CreateBTree(BTree &T)
+{
+     char c;
+     cin>>c;
+
+    if(c == '#')
+    {
+	T = NULL;
+    }
+    else
+    {
+	T = (BTree)malloc(sizeof(BTNode));
+	T->c_value = c;
+	
+	CreateBTree(T->lchild);
+
+	CreateBTree(T->rchild);
+    }
+}
+
+void print(BTree T)
+{
+     if(T->c_value != '#')
+     {
+	cout<<T->c_value<<endl;
+     }
+}
+
+void PreOrder(BTree T)
+{
+     if(T != NULL)
+     {
+	print(T);
+	PreOrder(T->lchild);
+	PreOrder(T->rchild);   
+     }
+}
+
+void InOrder(BTree T)
+{
+     if (T != NULL)
+     {
+	InOrder(T->lchild);
+	print(T);
+	InOrder(T->rchild);
+     }
+}
+
+void PostOrder(BTree T)
+{
+     if (T != NULL)
+     {
+	PostOrder(T->lchild);
+	PostOrder(T->rchild);
+	print(T);
+     }
+}
+
+//Get the tree's node sum
+
+int GetNodeSum(BTree T)
+{
+    if(T != NULL)
+	return (GetNodeSum(T->lchild) + GetNodeSum(T->rchild) + 1);
+}
+
+int GetTreeDeep(BTree T)
+{
+    if(T != NULL)
+	return (GetTreeDeep(
+}
 
 
 int main(void)
 {
+    BTree  Ptr;
+    CreateBTree(Ptr);
     
-    cout<<"the end"<<endl;
+/*    PreOrder(Ptr);
+    cout<<"\n"<<endl;
+    InOrder(Ptr);
+    cout<<"\n"<<endl;
+    PostOrder(Ptr);
+//    cout<<"\n"<<endl;
+*/
+     int i = GetNodeSum(Ptr);
+     cout<<i<<endl;
     return 0;
 }
+
