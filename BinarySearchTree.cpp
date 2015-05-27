0474e65d0621ade09f71bcb8fc71323fba187108
hustaoyang <t.yang.hust@gmail.com>
2015-05-27 16:19:37 +0800

    add avltree

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -64,21 +64,95 @@ void InOrderTree(BTree T)
     }
 }
 
+//The sum of Node in the k level
+int  GetNodeNumKLevel(BTree T, int k)
+{   
+    if(T == NULL)
+	return 0;
+    if(k < 1)
+	return 0;
+    if(k == 1)
+	return 1;
+    
+    int leftSum = GetNodeNumKLevel(T->lchild, k-1);
+    int rightSum = GetNodeNumKLevel(T->rchild, k-1);
+    return (leftSum + rightSum);
+}
+
+int GetNodeNumLeaf(BTree T)
+{
+    if(T == NULL)
+	return 0;
+    if(T->lchild == NULL && T->rchild == NULL)
+	return 1;
+
+    int leftSum = GetNodeNumLeaf(T->lchild);
+    int rightSum = GetNodeNumLeaf(T->rchild);
+    return (leftSum + rightSum);
+}
+
+bool StructureCmp(BTree T1, BTree T2)
+{
+    if(T1 == NULL && T2 == NULL)
+	return true;
+    if(T1 != NULL || T2 != NULL)
+	return false;
+
+    bool leftbool = StructureCmp(T1->lchild, T2->lchild);
+    bool rightbool = StructureCmp(T1->rchild, T2->rchild);
+    
+    return (leftbool && rightbool);
+}
+
+bool AVLTree(BTree T)
+{
+    if(T == NULL)
+	return true;
+    int leftLevel = AVLTree(T->lchild) + 1;
+    int rightLevel = AVLTree(T->rchild) + 1;
+    int k = (leftLevel - rightLevel) >= 0 ? (leftLevel - rightLevel) : (rightLevel - leftLevel);
+
+    if ( k <= 1 && k >= 0)
+	return true;
+    else 
+	return false;
+}
 
 int main(void)
 {
-     BTree Ptr = NULL;
+     BTree Ptr1 = NULL;
      int  A[10] = {15, 9, 7, 5, 12, 25, 17, 29, 27, 35};
-     
+     int  B[10] = {15, 9, 7, 5, 12, 25, 17, 29, 20, 35};
+      
+     BTree Ptr2 = NULL;
+     BTree Ptr = NULL;
+
      int i;
      for(i = 0; i< 10; i++)
-	InsertNode(Ptr, A[i]);
+     {
+      	//InsertNode(Ptr1, A[i]);
+	//InsertNode(Ptr2, B[i]);
+        InsertNode(Ptr, i+1);
+     }
      
-     PreOrderTree(Ptr);
+     //PreOrderTree(Ptr);
 
      //cout<<"\n"<<endl;
 
      //InOrderTree(Ptr);
+     //int sum = GetNodeNumKLevel(Ptr, 3);
+     //int sum = GetNodeNumLeaf(Ptr);
+     //cout<<sum<<endl;
+     /*
+     if(StructureCmp(Ptr1, Ptr2))
+	cout<<"The structure of Ptr1 and Ptr2 are the same"<<endl;
+     else
+	cout<<"The structure of Ptr1 and Ptr2 aren't the same"<<endl;
+     */
 
+      if(AVLTree(Ptr))
+	cout<<"yes"<<endl;
+      else
+	cout<<"no"<<endl;  
      return 0;
 }
