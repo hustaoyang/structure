079e978cf77bdcd7eea46ee9512b66ca9f6ab5a7
hustaoyang <t.yang.hust@gmail.com>
2015-08-12 18:00:48 +0800

    add stack

--- BinaryTree/RebuildBinaryTree.cpp
+++ BinaryTree/RebuildBinaryTree.cpp
@@ -2,9 +2,11 @@
 #include <exception>
 #include <new>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+/*
 typedef struct BTNode 
 {
     int i_value;
@@ -36,8 +38,8 @@ BTree construct_tree(int *PreOrderStart, int *PreOrderEnd,
         if (InOrderStart == InOrderEnd && *PreOrderStart == *InOrderStart)
             return pRoot;
         else
-            //return NULL;
-           throw new exception("Invalid input.");
+            return NULL;
+           //throw new exception("Invalid input.");
     }
 
     int *now = InOrderStart;
@@ -45,8 +47,8 @@ BTree construct_tree(int *PreOrderStart, int *PreOrderEnd,
         ++now;
 
     if (now == InOrderEnd && *now != *InOrderEnd)
-        //return NULL;
-        throw new exception("Invalid input.");
+        return NULL;
+        //throw new exception("Invalid input.");
 
     int left_len = now - InOrderStart;
     int *left_preOrderEnd = PreOrderStart + left_len;
@@ -65,25 +67,92 @@ BTree construct_tree(int *PreOrderStart, int *PreOrderEnd,
 
     
 }
+*/
 
-void print(BTree tree)
+// Definition for binary tree
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	//TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    struct TreeNode* reConstructBinaryTree(vector<int> pre,vector<int> in) {
+        int len1 = pre.size();
+        int len2 = in.size();
+        
+        if (len1 < 0 || len1 != len2)
+            return NULL;
+        
+        TreeNode* re_node = do_reConstruct(pre, 0, len1 - 1, in, 0, len1 - 1);
+        
+        return re_node;
+    }   
+private:
+    static TreeNode* do_reConstruct(vector<int>& pre, int pre_start, int pre_end, vector<int>& in, int in_start, int in_end)
+    {
+        TreeNode* root = new TreeNode();
+        root->val = pre[pre_start];
+        root->left = root->right = NULL;
+        
+        if (pre_start == pre_end)
+        {
+        	if (in_start == in_end && in[in_start] == in[in_end])
+                return root;
+            else
+                return NULL;
+        }
+        
+        int move = in_start;
+        while (move <= in_end && in[move] != pre[pre_start])
+            ++move;
+        
+        if (move == in_end && in[move] != pre[pre_start])
+            return NULL;
+       	int len = move - in_start; 
+        if (len  > 0)
+        {
+        	root->left = do_reConstruct(pre, pre_start + 1, pre_start + len, in, in_start, move - 1);
+        }
+		if (in_end - move > 0)
+		{
+			root->right = do_reConstruct(pre, pre_start + len + 1, pre_end, in, move + 1, in_end);
+		}	
+		
+		return root;
+	}
+};
+
+void print(TreeNode* tree)
 {
     if (NULL == tree)
         return ;
 
-    print(tree->lchild);
+    print(tree->left);
    // cout<<tree->i_value<<" ";
-    print(tree->rchild);
-    cout<<tree->i_value<<" ";
+    print(tree->right);
+    cout<<tree->val<<" ";
 }
+
+
 
 int main(void)
 {
-    int InOrder[] = {5, 7, 9, 12, 15, 17, 25, 27, 29, 35};
-    int PreOrder[] = {15, 9, 7, 5, 12, 25, 17, 29, 27, 35};
+    //int InOrder[] = {5, 7, 9, 12, 15, 17, 25, 27, 29, 35};
+    //int PreOrder[] = {15, 9, 7, 5, 12, 25, 17, 29, 27, 35};
     
-    BTree rebuild_tree = RebuildBinaryTree(InOrder, PreOrder, 10);
+	int PreOrder[] = {1, 2, 4, 7, 3, 5, 6, 8};
+	int InOrder[] = {4, 7, 2, 1, 5, 3, 8, 6};
 
+	vector<int> ino(InOrder, InOrder + 8);
+	vector<int> pre(PreOrder, PreOrder + 8);
+
+    //BTree rebuild_tree = RebuildBinaryTree(InOrder, PreOrder, 10);
+	
+	Solution S;
+	TreeNode* rebuild_tree = S.reConstructBinaryTree(ino, pre);
     if (rebuild_tree != NULL)
         print(rebuild_tree);
     return 0; 
